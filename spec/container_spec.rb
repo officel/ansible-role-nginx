e86ca2b558d437325c7b03d3afa7439d6d41e46e
officel <nishimuray@gmail.com>
2016-08-12 15:07:15 +0900

    add util template

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -19,3 +19,19 @@ end
 describe service('nginx') do
   it { should be_enabled }
 end
+
+# one or more think?
+
+describe file('/etc/nginx/conf.d/include') do
+  it { should be_directory }
+end
+
+describe file('/etc/nginx/conf.d/include/healthcheck_no_log.inc') do
+  it { should be_file }
+end
+describe file('/etc/nginx/conf.d/include/geo_allow_ip.inc') do
+  it { should be_file }
+end
+describe file('/etc/nginx/conf.d/include/limit_ip.inc') do
+  it { should be_file }
+end
